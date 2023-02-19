90bb21052c3590fcbf8895910a851eefb4ee6006
Shin.Yamamoto <yamashin0123@outlook.jp>
2023-02-19 23:24:06 +0900

    fix: remove jest&cypress and unnecessary file

--- src/pages/article/[slug].tsx
+++ src/pages/article/[slug].tsx
@@ -4,9 +4,6 @@ import type {
   InferGetStaticPropsType,
 } from 'next';
 
-import { Meta } from '@/layouts/Meta';
-import { Main } from '@/templates/Main';
-
 type IBlogUrl = {
   slug: string;
 };
@@ -32,7 +29,7 @@ export const getStaticProps: GetStaticProps<IBlogUrl, IBlogUrl> = async ({
 
 const Blog = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <Main meta={<Meta title={props.slug} description="Lorem ipsum" />}>
+    <>
       <h1 className="capitalize">{props.slug}</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eos
@@ -40,7 +37,7 @@ const Blog = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
         iste expedita cupiditate a quidem culpa eligendi, aperiam saepe dolores
         ipsum!
       </p>
-    </Main>
+    </>
   );
 };
 
